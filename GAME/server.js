84309ca2761995fc9878eec771072c2fe1155326
jorget9 <jt2892@nyu.edu>
2018-12-10 21:08:21 -0500

    Add files via upload

---
+++ GAME/server.js
@@ -0,0 +1,154 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+app.use('/css',express.static(__dirname + '/css'));
+app.use('/js',express.static(__dirname + '/js'));
+app.use('/assets',express.static(__dirname + '/assets'));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/lobby.html');
+});
+
+app.use(express.static('/'))
+
+
+server.lastPlayderID = 0;
+
+server.listen(process.env.PORT || 8081,function(){
+    console.log('Listening on '+server.address().port);
+});
+var rooms = ['Please Pick A Room','room1','room2','room3'];
+var usernames = {};
+
+
+io.on('connection',function(socket){
+
+    
+
+
+    socket.on('pickrooms',function(name){
+        socket.username = name;
+        
+    });
+    // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+    // Whenever the server emits 'login', log the login message
+  socket.on('login', function (data) {
+    connected = true;
+    // Display the welcome message
+    var message = "Welcome to the Game Server ";
+    log(message, {
+      prepend: true
+    });
+    addParticipantsMessage(data);
+  });
+    
+    // when the client emits 'adduser', this listens and executes
+    socket.on('adduser', function(username){
+        // store the username in the socket session for this client
+        socket.username = username;
+        // store the room name in the socket session for this client
+        socket.room = 'Please Pick A Room';
+        // add the client's username to the global list
+        usernames[username] = username;
+        // send client to room 1
+        socket.join('Please Pick A Room');
+        // echo to client they've connected
+        socket.emit('updatechat', 'SERVER', 'you have connected to room1');
+        // echo to room 1 that a person has connected to their room
+        socket.broadcast.to('Please Pick A Room').emit('updatechat', 'SERVER', username + ' has connected to this room');
+        socket.emit('updaterooms', rooms, 'Please Pick A Room');
+        var room = io.sockets.adapter.rooms['Please Pick A Room'];
+        var number = room.length;
+        console.log(number);
+    });
+    
+    // when the client emits 'sendchat', this listens and executes
+    socket.on('sendchat', function (data) {
+        // we tell the client to execute 'updatechat' with 2 parameters
+        io.sockets.in(socket.room).emit('updatechat', socket.username, data);
+    });
+
+        
+    
+    socket.on('switchRoom', function(newroom){
+        socket.leave(socket.room);
+        socket.join(newroom);
+        socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+        // sent message to OLD room
+        socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
+        // update socket session room title
+        socket.room = newroom;
+        socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
+        socket.emit('updaterooms', rooms, newroom);
+        var destination = __dirname+'/game'+newroom+'.html';
+        socket.emit('redirect', destination);
+    });
+    
+
+    // when the user disconnects.. perform this
+    socket.on('disconnect', function(){
+        // remove the username from global usernames list
+        delete usernames[socket.username];
+        // update list of users in chat, client-side
+        io.sockets.emit('updateusers', usernames);
+        // echo globally that this client has left
+        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+        socket.leave(socket.room);
+    });
+
+    socket.on('newplayer',function(){
+        socket.player = {
+            id: server.lastPlayderID++,
+            x: randomInt(100,400),
+            y: randomInt(100,400)
+        };
+        socket.emit('allplayers',getAllPlayers());
+        socket.broadcast.emit('newplayer',socket.player);
+
+        socket.on('click',function(data){
+            console.log('click to '+data.x+', '+data.y);
+            socket.player.x = data.x;
+            socket.player.y = data.y;
+            io.emit('move',socket.player);
+        });
+
+        socket.on('disconnect',function(){
+            io.emit('remove',socket.player.id);
+        });
+    });
+
+    socket.on('test',function(){
+        console.log('test received');
+    });
+});
+
+function getAllPlayers(){
+    var players = [];
+    Object.keys(io.sockets.connected).forEach(function(socketID){
+        var player = io.sockets.connected[socketID].player;
+        if(player) players.push(player);
+    });
+    return players;
+}
+
+function randomInt (low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
